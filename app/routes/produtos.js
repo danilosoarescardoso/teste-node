bc98cad685838339da6e49f539522009ecc692db
Danilo Soares Cardoso <dsc.soares@gmail.com>
2017-07-10 03:01:13 +0100

    Setting up the MySQL database

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,19 @@
 
 module.exports = function(app){ 
 	app.get('/produtos', function(req, res){
+		var mysql = require('mysql');
+
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'node-teste_database'
+		})
+
+		connection.query('select * from livros');
+		connection.end();
+
+		//consulta
 		res.render("produtos/lista");
 	});
 }
