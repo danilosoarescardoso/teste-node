f2e164cb7067297c32d431701792e8e4ed3c3b8b
Danilo Soares Cardoso <dsc.soares@gmail.com>
2017-07-11 23:13:48 +0100

    Isolating the database access

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,14 +1,9 @@
+var dbConnection = require('../infra/dbConnection');
 
 module.exports = function(app){ 
 	app.get('/produtos', function(req, res){
-		var mysql = require('mysql');
-
-		var connection = mysql.createConnection({
-			host: 'localhost',
-			user: 'root',
-			password: '',
-			database: 'nodeteste_database'
-		});
+		
+		var connection = dbConnection();
 
 		connection.query('select * from produtos', function(err, results){
 			res.render('produtos/lista',{lista:results});
